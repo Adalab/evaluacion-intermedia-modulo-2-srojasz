bab6f92301d6c8fe96ecefd316fddd51f9ce6dfe
srojasz <srojasz@gmail.com>
2019-11-20 10:05:18 +0100

    Fix problems

---
+++ js/main.js
@@ -0,0 +1,42 @@
+"use strict";
+
+// Generar número aleatorio
+
+const max = 100;
+debugger;
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
+}
+
+// Prevent button
+
+const tryButton = document.querySelector(".form__button");
+
+function stopForm(ev) {
+  ev.preventDefault();
+}
+
+tryButton.addEventListener("click", stopForm);
+
+// Comparar randomNumber y userNumber cuando se pincha el botón de prueba
+
+const userNumberElement = document.querySelector(".js-form__input");
+
+const clue = document.querySelector(".js-clue");
+
+const randomNumber = getRandomNumber(max);
+
+function handleGame(ev) {
+  const userNumberValue = parseInt(userNumberElement.value);
+  userNumberValue;
+  if (randomNumber === parseInt(userNumberValue)) {
+    clue.innerHTML = "Has ganado campeona!!!";
+  } else if (randomNumber < parseInt(userNumberValue)) {
+    clue.innerHTML = "Demasiado alto";
+  } else if (randomNumber > parseInt(userNumberValue)) {
+    clue.innerHTML = "Demasiado bajo";
+  }
+}
+
+tryButton.addEventListener("click", handleGame);
+console.log(randomNumber);
